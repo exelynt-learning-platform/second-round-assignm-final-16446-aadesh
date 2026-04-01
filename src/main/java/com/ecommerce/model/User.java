03705226247231d3f1fd29274adaf1cfabb25bd3
Aadesh Dev <student@ecommerce.com>
2026-04-01 19:33:52 +0530

    feat: initialize Spring Boot project with dependencies

---
+++ src/main/java/com/ecommerce/model/User.java
@@ -0,0 +1,38 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
